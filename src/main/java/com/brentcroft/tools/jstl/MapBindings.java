64e9792a2b8a13bb4ca1aefd7e07e4ca9b787740
Alaric Dobson <alaric@brentcroft.com>
2022-12-28 11:55:14 +0000

    Fix auto-scope.

--- src/main/java/com/brentcroft/tools/jstl/MapBindings.java
+++ src/main/java/com/brentcroft/tools/jstl/MapBindings.java
@@ -77,7 +77,11 @@ public class MapBindings extends LinkedHashMap< String, Object > implements Bind
     @Override
     public Object get( Object a )
     {
-        return super.containsKey( a ) ? super.get( a ) : parent != null && parent.containsKey( a ) ? parent.get( a ) : null;
+        return super.containsKey( a )
+               ? super.get( a )
+               : parent != null
+                    ? parent.get( a )
+                    : null;
     }
 
 
@@ -110,105 +114,4 @@ public class MapBindings extends LinkedHashMap< String, Object > implements Bind
             return super.toString() + ( "; " + parent.toString() );
         }
     }
-
-
-    /**
-     * Renders all values that are EL Templates.
-     * <p>
-     * Since templates may contain references to other entries,
-     * and such entries may be templates themselves,
-     * it's  necessary to detect entry values with templates, render them,
-     * and detect and render again, until no EL Templates remain,
-     * or more than 100 attempts have been made,
-     * when a RuntimeException is thrown.
-     * <p>
-     * Any templates that fail to compile are indicated in the exception message.
-     *
-     * @param textExpander a TextExpander to implement the rendering
-     */
-    public void renderAllTemplates( TextExpander textExpander )
-    {
-        List< ELTemplateException > ignoredExceptions = new ArrayList<>();
-
-        boolean changed;
-
-        int loopIndex = 0;
-        do
-        {
-            loopIndex++;
-
-            if ( loopIndex > 100 )
-            {
-                throw new RuntimeException(
-                        format( "Too many loops [100] while expanding properties: Is there a circular definition?; %s %s",
-                                this,
-                                ignoredExceptions.isEmpty()
-                                ? ""
-                                : ignoredExceptions
-                        ) );
-            }
-
-            // set false at beginning of each run
-            changed = false;
-
-            // clear for action
-            ignoredExceptions.clear();
-
-
-            for ( String key : keySet() )
-            {
-                final Object value = get( key );
-
-                // and therefore not null
-                if ( value instanceof String )
-                {
-                    String oldValue = ( String ) value;
-
-                    // if not a template then doesn't need expanding
-                    if ( ELTemplateManager.hasTemplate( oldValue ) )
-                    {
-                        try
-                        {
-                            String newValue = textExpander.expandText( ( String ) value, this );
-
-                            // if has changed value then loop
-                            if ( ! oldValue.equals( newValue ) )
-                            {
-                                put( key, newValue );
-
-                                changed = true;
-                            }
-                            // if hasn't changed value but is still a template
-                            else if ( ELTemplateManager.hasTemplate( newValue ) )
-                            {
-                                throw new RuntimeException( format( "Detected stationary EL expression (i.e. circularity [%s]): key=[%s], value=[%s]:\n %s",
-                                        loopIndex,
-                                        key,
-                                        newValue,
-                                        this ) );
-                            }
-                        }
-                        catch ( ELTemplateException elte )
-                        {
-                            // try again later
-                            ignoredExceptions.add( elte );
-                        }
-                    }
-                }
-            }
-        } while ( changed );
-
-        if ( ! ignoredExceptions.isEmpty() )
-        {
-            throw new RuntimeException(
-                    format( "Some entries generated EL Template exceptions:%n bindings=%s%n%s",
-                            this,
-                            ignoredExceptions
-                                    .stream()
-                                    .map( Throwable::getMessage )
-                                    .collect( Collectors.joining( "\n " ) )
-                    ) );
-        }
-    }
-
 }
